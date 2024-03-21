8daec5df33a20b19a0a6212ee4c5091c152d0c4a
Frankysv08 <frankysv97@gmail.com>
2024-03-21 08:13:12 +0530

    once more changed

--- src/test/java/com/Cucumber_learning/JustTrail.java
+++ src/test/java/com/Cucumber_learning/JustTrail.java
@@ -2,7 +2,11 @@ package com.Cucumber_learning;
 
 public class JustTrail {
 
+	public void letsDoIt() {
+		System.out.println("How is it?");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello I got it");
 	}
+	
 }
